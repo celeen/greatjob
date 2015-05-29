228d0abb680c7502e046f997306aed25fc6c0844
celeen <celeenrusk@gmail.com>
2015-05-29 15:45:04 -0500

    frenzied changes

--- greatjob/__init__.py
+++ greatjob/__init__.py
@@ -5,8 +5,8 @@ main_image = os.path.dirname(os.path.abspath(__file__)) + "/thumbsup.png"
 sparkle_image = os.path.dirname(os.path.abspath(__file__)) + "/sparkle.png"
 
 def main():
-	cwd = os.getcwd()
-	os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/proj/")
+	# cwd = os.getcwd()
+	# os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/proj/")
 	# os.system('make clean')
 	# os.system('make install')
 
--- setup.py
+++ setup.py
@@ -1,28 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages, Extension
 
-# proj = Extension('proj', sources=[ 'proj/main', 
-#                                     'proj/main.m', 
-#                                     'proj/Makefile', 
-#                                     'proj/build/imageleap', 
-#                                     'proj/imageleap.xcodeproj/project.xcodeworkspace/xcshareddata/imageleap.xccheckout', 
-#                                     'proj/imageleap.xcodeproj/project.xcodeworkspace/contents.xcworkspace', 
-#                                     'proj/imageleap.xcodeproj/project.pbxproj/'], 
-#                         depends=['proj/main', 
-#                                     'proj/main.m', 
-#                                     'proj/Makefile', 
-#                                     'proj/build/imageleap', 
-#                                     'proj/imageleap.xcodeproj/project.xcodeworkspace/xcshareddata/imageleap.xccheckout', 
-#                                     'proj/imageleap.xcodeproj/project.xcodeworkspace/contents.xcworkspace', 
-#                                     'proj/imageleap.xcodeproj/project.pbxproj/'])
-
 setup(name='greatjob',
-      version='1.0',
+      version='1.1.1',
       description='Congratulations',
       author='Celeen Rusk',
       author_email='celeenrusk@gmail.com',
       liscense="MIT",
-      # ext_modules=[proj],
       packages=find_packages(),
       package_data={
             '': ['imageleap', 'sparkle.png', 'thumbsup.png', 'proj/*']
